97e0f2aa40b348c099e8e290c9ea7a3e800d4681
Himanshu Sain <sainhimanshu9@gmail.com>
2019-09-19 15:09:03 +0530

    gender function added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
+     
+    gender(event){
+      var gender = this.state.gender;
+      if(event.target.value==='male'){
+         
+          gender='male';
+          this.setState({gender: gender}, function () {
+            console.log(this.state.gender);
+            
+          });
+      }
+      else if (event.target.value==='female'){
+        
+        gender='female';
+        this.setState({gender: gender}, function () {
+          console.log(this.state.gender);
+          
+        });
+      }
+    }
   calculateBMR(event){
     event.preventDefault();
     var weight = this.refs.weight.value;
     var height = this.refs.height.value;
     var age = this.refs.age.value;
-    var bmr = 66.47 + (6.24 * weight * 2.2) + (12.7 * height) - (6.755 * age);
+    if(this.state.gender==='male'){
+    var bmr = (13.397 * weight) + (4.799 * height) - (5.677 * age)+ 88.362;
+    }
+    else if(this.state.gender==='female'){
+      bmr = (9.247 * weight) + (3.098 * height) - (4.330 * age)+ 447.593;
+    }
     var maintainCalories = bmr * 1.3;
-    var deficit = 0.05* maintainCalories;
-    var dailyCalories = Math.floor(maintainCalories - deficit);
+    
+    var dailyCalories = Math.floor(maintainCalories - 500);
     this.setState({dailyCalories: dailyCalories});
     var protein = Math.floor((1/8)*dailyCalories);
     this.setState({protein: protein});
@@ -27,11 +51,12 @@ class App extends Component {
       dailyCalories: 0,
       protein: 0,
       carbohydrate: 0,
-      fat: 0
+      fat: 0,
+      gender: ''
      
     };
     this.calculateBMR = this.calculateBMR.bind(this);
-    
+    this.gender = this.gender.bind(this);
   }
     
 
@@ -40,9 +65,12 @@ class App extends Component {
     return (
       <div>
             <form onSubmit={this.calculateBMR} >
-            <input type="number" ref="weight" placeholder="enter you weight in Kg's" />
-            <input type="number" ref="height" placeholder="enter you height in inches" />
+            <input type="float" ref="weight" placeholder="enter you weight in Kg's" />
+            <input type="float" ref="height" placeholder="enter you height in cms" />
             <input type="number" min="1" ref="age" placeholder="enter your age" />
+            <input type="radio" name="gender" value="male" onChange={this.gender} />Male
+            <input type="radio" name="gender" value="female" onChange={this.gender} />Female
+
             <input type="submit" value="Submit" />
             </form>
            <h1>your daily Calories are: {this.state.dailyCalories} Calories</h1>
